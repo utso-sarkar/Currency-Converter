e65498cac6ef77442d37c68984005212a4e78e43
Utso-Sarkar <devutso428@gmail.com>
2024-01-15 09:47:08 +0600

    added done

---
+++ app.js
@@ -0,0 +1,10 @@
+const BaseUrl ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json"
+const dropdowns=document.querySelectorAll('.dropdown select');
+
+for(code in countryList){
+    console.log(code);
+}
+
+// for (let select of dropdowns){
+
+// }
